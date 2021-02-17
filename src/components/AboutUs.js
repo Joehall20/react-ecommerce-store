b3e6bf3de56bc4ea9b62151c707f2eae7a3b4685
Lankybean17 <69683144+Lankybean17@users.noreply.github.com>
2021-02-17 12:34:36 +0000

    Mail to button

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Title from './Title';
+import {CartButton} from './Button';
+
 
 export default class AboutUs extends Component {
     render() {
@@ -7,7 +9,16 @@ export default class AboutUs extends Component {
             <div className="py-5">
                 <Title title="About Us"/>
                 <div className="body">
+
                     The finest armour in the galaxy
+
+                    <div>
+                    <CartButton prodbtn onClick={()=>{
+                        window.location.href="mailto:TheBrushWarrior@hotmail.com"
+                            }}>
+                        <span className="title">Want Something Custom?</span>
+                    </CartButton>
+                    </div>
                 </div>
             </div>
         )
